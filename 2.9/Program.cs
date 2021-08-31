cb6bfd5c970664b475da35fa7d96f6b380f8f51c
Adrian Wikström <04advaa@skola.lidkoping.se>
2021-08-31 08:32:27 +0200

    Add project files.

---
+++ 2.9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _2._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hur långt hoppade Elin?");
+            double längd1 = double.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Hur långt hoppade Alma?");
+            double längd2 = double.Parse(Console.ReadLine());
+            Console.Clear();
+
+            double längdLängre = längd1 - längd2;
+
+            Console.WriteLine("Elin hoppade " + längdLängre + " cm än Alma");
+
+        }
+    }
+}
